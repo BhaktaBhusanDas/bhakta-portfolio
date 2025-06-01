55109ac2e780ce714833378b5196ebe0e76b3bed
Bhakta Bhusan Das <bhaktabhaktabhakta@gmail.com>
2025-06-01 14:13:55 +0530

    Fixing the paragraph colour and Datadog title

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,14 @@ export default function Home() {
   return (
     <section className="text-center py-20">
       <h1 className="text-5xl font-bold mb-4">Bhakta Bhusan Das</h1>
-      <p className="text-xl text-gray-700 mb-8">
+      <p className="text-xl mb-8">
         I build automated, observable, self-healing infrastructure with terraform, Kubernets, CI/CD and Datadog.
       </p>
       <div className="inline-flex space-x-4">
         <span>🧱 Terraform</span>
         <span>🐳 Docker</span>
         <span>🤖 Github</span>
-        <span>📈 Github</span>
+        <span>📈 Datadog</span>
       </div>
     </section>
   );
